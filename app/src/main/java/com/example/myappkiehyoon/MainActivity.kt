e457b11548bd72260c55a7fb132d64bcdab8747b
KIEHYOON <feverishkk@gmail.com>
2020-05-26 23:47:21 +0900

    First Commit Main, ViewPageAdapter and both of xml

---
+++ app/src/main/java/com/example/myappkiehyoon/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.myappkiehyoon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+
+class MainActivity : AppCompatActivity() {
+
+    internal lateinit var viewPager: ViewPager
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        viewPager = findViewById(R.id.viewpager) as ViewPager
+
+        val adapter = ViewPageAdapter(this)
+        viewPager.adapter = adapter
+    }
+}
